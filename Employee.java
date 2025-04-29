f19d0cdd1f36be2f9fe90c726175f0253f708638
Komal-Chinnu <komalpawar2k13@gmail.com>
2025-04-29 08:40:18 +0530

    Saved

---
+++ Employee.java
@@ -0,0 +1,26 @@
+package javaprogramming;
+
+public class Employee {
+
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name , int id , double salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayInfo(){
+        System.out.println("Employee ID :" +id);
+        System.out.println("Employee Name :" +name);
+        System.out.println("Salary :" +salary);
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Komal",101 , 53000.00);
+
+        e1.displayInfo();
+    }
+}
